648088b3e88bd1077eb5f5f562c9e7a6f98c029b
jlebduska <j.lebduska@scia.net>
2021-09-22 17:14:07 +0200

    strict mode on

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,23 +5,18 @@ import { BrowserModule } from "@angular/platform-browser"
 import { RouterModule } from "@angular/router"
 
 import { AppComponent } from "./app.component"
-import { FetchDataComponent } from "./fetch-data/fetch-data.component"
 import { HomeComponent } from "./home/home.component"
 import { NavMenuComponent } from "./nav-menu/nav-menu.component"
 
 @NgModule({
-  declarations: [AppComponent, NavMenuComponent, HomeComponent, FetchDataComponent],
+  declarations: [AppComponent, NavMenuComponent, HomeComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(
-      [
-        { path: "", component: HomeComponent, pathMatch: "full" },
-        { path: "fetch-data", component: FetchDataComponent },
-      ],
-      { relativeLinkResolution: "legacy" }
-    ),
+    RouterModule.forRoot([{ path: "", component: HomeComponent, pathMatch: "full" }], {
+      relativeLinkResolution: "legacy",
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
